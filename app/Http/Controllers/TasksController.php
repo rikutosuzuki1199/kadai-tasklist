aa9dd73fa40b81549aed8b04b57bfd019402087f
rikuto.suzuki <suzuki.rikuto@servithink.co.jp>
2024-07-04 03:45:09 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -37,6 +37,11 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required'
+        ]);
+        
         $task = new Task;
         $task->status = $request->status;
         $task->content = $request->content;
@@ -74,6 +79,11 @@ class TasksController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required'
+        ]);
+        
         $task = Task::findOrFail($id);
         $task->status = $request->status;
         $task->content = $request->content;
