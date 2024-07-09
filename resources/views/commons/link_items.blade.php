71bcc952e7a0e017a53a68d7e255455389dd2637
rikuto.suzuki <suzuki.rikuto@servithink.co.jp>
2024-07-09 02:06:51 +0000

    top page

---
+++ resources/views/commons/link_items.blade.php
@@ -0,0 +1,12 @@
+@if (Auth::check())
+    {{-- 新規タスク追加ページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('tasks.create') }}">新規タスクの追加</a></li>
+    {{-- ログアウトへのリンク --}}
+    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
+@else
+    {{-- ユーザー登録ページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('register') }}">Signup</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログインページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('login') }}">Login</a></li>
+@endif
